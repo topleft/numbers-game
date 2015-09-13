b274441a11b69360fab267b178d64c6db98d4396
topleft <pete.james.jeffryes@gmail.com>
2015-09-13 16:06:42 -0600

    more sudo code

--- models/ai.js
+++ models/ai.js
@@ -5,12 +5,17 @@
 var Ai = function(name, strategy){
   Player.call(this, name);
   this.strategy  = null; //will be used to set a playing style, ie aggressive
+  this.opponentMoves = []; // indoex 0 will be first move
 }
 
 Ai.prototype = new Player(Ai.name);
 
 
-// total points available =
+Ai.prototype.updateOpponentMoves= function(move){
+  this.opponentMoves.push(move);
+}
+
+// might need an discoverOppMove if we only have access to a resulting array
 
 Ai.prototype.chooseNumber = function(opponentNumbers){
 
@@ -30,9 +35,11 @@ Ai.prototype.chooseNumber = function(opponentNumbers){
       choice = 8;
       return choice;
     }
-    else {}
+    else {// run evaluateChoices, if array is returned select high number}
+    }
 
   };
+
   if (this.strategy === 'mild'){
   // mild: never give up 2 points, go for score at good probabilities
     if (this.turnCount === 0){
@@ -47,9 +54,10 @@ Ai.prototype.chooseNumber = function(opponentNumbers){
       choice = 4;
       return choice;
     }
-    else {}
-
+    else {// run evaluateChoices, if array is returned select middle number}
+    }
   };
+
   if (this.strategy === 'aggressive'){
   // aggressive: go for points, use low numbers first
     if (this.turnCount === 0){
@@ -64,37 +72,43 @@ Ai.prototype.chooseNumber = function(opponentNumbers){
       choice = 2;
       return choice;
     }
-    else {}
+    else {// run evaluateChoices, if array is returned select low number
+    };
+  }
+  this.turnCount ++;
+};
 
-  };
-  this.turnCount ++
-}
-
-Ai.prototype.calculateChoiceWeight = function(choice, opponentNumbers){
+Ai.prototype.calculateChoiceWeight = function(choice, opponentNumbers) {
 
   var choiceWeight;
 
   for (var i = 0; i < opponentNumbers.length; i++) {
     // score 2
-    if (opponentNumbers[i]+1 === choice) {choiceWeight += 2};
+    if (opponentNumbers[i]+1 === choice) {choiceWeight += 2}
     // give 2
-    else if (opponentNumbers[i]-1 === choice) {choiceWeight -= 2};
+    else if (opponentNumbers[i]-1 === choice) {choiceWeight -= 2}
     // score 1
-    else if (opponentNumbers[i] > choice) {choiceWeight ++};
+    else if (opponentNumbers[i] > choice) {choiceWeight ++}
     // give 1
-    else if (opponentNumbers[i] < choice) {choiceWeight --};
+    else if (opponentNumbers[i] < choice) {choiceWeight --}
     }
 
-  return {choice: choiceWeight};
+  return {choice: choiceWeight}; // higher choiceWeight = more likely to score with that choice
 };
 
-Ai.prototype.evaluateChoices = function(opponentNumbers){
+Ai.prototype.evaluateChoices = function(opponentNumbers) {
   var choices = [];
+
   for (var i = 0; i < this.numbers.length; i++) {
     choices.push(this.calculateChoiceWeight(this.numbers[i], opponentNumbers)
-  };
+  }
   // return choice with highest choiceWeight, if duplicate choiceWeights return array of choices
-}
+};
+
+Ai.prototype.evaluateOpponentStrategy = function(opponentNumbers){
+  // analyze opponents moves and apply a def., mild, or aggressive strategy
+};
+
 
 
 
