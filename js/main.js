5546c19db3853a3373fcfe9edf5e3b0bfbad4854
topleft <pete.james.jeffryes@gmail.com>
2015-09-14 17:12:28 -0600

    cleaned up main.js, took out console.logs

--- js/main.js
+++ js/main.js
@@ -4,12 +4,9 @@ $(document).on('ready', function() {
   console.log('sanity check!');
 
   var game = new Game("ai", 10, "Pete", "AI");
-  var count = 0;
 
   game.createPlayerNumbers();
-  while(!game.callTurn() || count< 15){
-    count ++;
-    console.log(count)
+  while(!game.callTurn()){
     game.callTurn();
   };
 });
